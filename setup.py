fc2ba240e2d2b064215358bad23fb478d4304228
Cameron Boulton <cameron@safecorp.com>
2019-05-08 10:55:28 -0700

    Version Python module.

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup
 
 setup(
     name='sceptre-resolver-aws-secrets-manager',
+    version="1.0.0",
     py_modules=['aws_secrets_manager'],
     entry_points={
         'sceptre.resolvers': [
